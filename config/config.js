c03cb2d5a65c888f8d957f857ca9fe9afb6ece3b
*vivek <vrvivekpc@gmail.com>
2024-02-03 10:04:33 +0530

    removed config folder

--- config/config.js
+++
@@ -1,8 +0,0 @@
-const sessionSecret = "net-ninja"
-const emailuser = "smtpp2002@gmail.com"
-const emailpassword ="saaw emmg svgr xzks"
-module.exports = {
-    sessionSecret,
-    emailuser,
-    emailpassword
-}
